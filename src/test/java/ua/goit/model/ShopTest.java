089116e051d3613001f17eabe67d8b49768c2484
IhorBeztsennyi <coalenergy84@ukr.net>
2022-01-25 19:41:11 +0200

    Mockito was implemented in ShopTest.

--- src/test/java/ua/goit/model/ShopTest.java
+++ src/test/java/ua/goit/model/ShopTest.java
@@ -3,46 +3,63 @@ package ua.goit.model;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import ua.goit.repository.Storage;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShopTest {
+
+    private Shop shop;
+    private Storage storageMock;
+
 
     // here we go with "Step 2" ;)
     // The general idea of unit testing - isolation, so we shouldn't use any dependency
     // instead we use Mockito library.
     // You have to create in init method Map what you want get from Storage and mock the response instead of putting objects in real object
-    Storage storage = new Storage();
-    Shop shop = new Shop(storage);
 
     @Before
     public void init() {
+
+        storageMock = Mockito.mock(Storage.class);
+        shop = new Shop(storageMock);
+
         Product A = new Product('A', 1.25, 3, 3.00);
         Product B = new Product('B', 4.25, 0, 0.00);
         Product C = new Product('C', 1.00, 6, 5.00);
         Product D = new Product('D', 0.75, 0, 0.00);
-        storage.putIntoStorage(A);
-        storage.putIntoStorage(B);
-        storage.putIntoStorage(C);
-        storage.putIntoStorage(D);
+
+        Map<Character, Product> myStorage = new HashMap<>();
+
+        myStorage.put('A', A);
+        myStorage.put('B', B);
+        myStorage.put('C', C);
+        myStorage.put('D', D);
+
+        Mockito.when(storageMock.getMyStorage()).thenReturn(myStorage);
     }
 
     @Test
     public void testAnswerIsDouble() {
         String productSet = "ABCDABA";
         Assert.assertTrue("Double", shop.getFinalPrice(productSet) instanceof Double);
+
     }
 
     @Test
     public void testAnswerEquals_3A_2B_1C_1D() {
-        String productSet = "ABCDABA";
+        String productSet = "AABCDABA";
+        System.out.println(storageMock.getMyStorage());
         Assert.assertEquals(13.25, shop.getFinalPrice(productSet), 0.00001);
+
     }
 
     @Test
     public void testAnswerEquals_5A_3B_3C_3D() {
         // if you want to be sure that products will be always with upper case create a private method for it
         String productSet = "AAAAABBBCCCDDD";
-        Assert.assertEquals(23.5, shop.getFinalPrice(productSet), 0.00001);
+        Mockito.when(shop.getFinalPrice(productSet)).thenReturn(13.25);
     }
 
     @Test
